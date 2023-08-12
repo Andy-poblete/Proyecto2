7d97741099ace0e029100c3269f0c217680c3e87
Andy-poblete <andy.poblete19@gmail.com>
2023-08-12 09:32:56 -0400

    Esta completo el proyecto

--- app.js
+++ app.js
@@ -1,144 +1,137 @@
-function validateForm() {
+function validateForm(){
+
+    //obtengo valores de los campos
     let email = document.getElementById('inputEmail').value;
-    let nombre = document.getElementById('inputNombre').value;
-    let numero = document.getElementById('inputNumero').value;
+    let name = document.getElementById('inputName').value;
+    let phone =document.getElementById('inputPhone').value;
+    //validar
+    if(email==""){
+        alert('El correo es requerido');
+        return false;
+    }else if(!email.includes("@")){
+        alert('El correo no es valido');
+        return false;
 
-    //a continuacion se van a validar los campos
-    if (email == "") {
-        alert("Uno de los campos esta vacio");
+    }
+    //validar el campo nombre
+    if(name==""){
+        alert('El nombre es requerido');
         return false;
     }
-    else if(!email.includes('@')){
-        alert("Debe agregar un @ al campo");
+    //validar el campo telefono
+    if(phone==""){
+        alert('El telefono es requerido');
         return false;
     }
-
-    else if (nombre == "") {
-        alert("Uno de los campos esta vacio");
-        return false;
-    }
-
-    else if (numero == "") {
-        alert("Uno de los campos esta vacio");
-        return false;
-    }
-
-    //se valida de que ningun dato este vacio
+    //Si pasa todas las validaciones
     return true;
+
+
 }
-
-function showData() {
+function showData(){
     let listPeople;
-
-    if (localStorage.getItem('listPeople') == null) {
-        listPeople = [];
+    if(localStorage.getItem('listPeople')==null){
+        listPeople=[];
+    }else{
+        listPeople = JSON.parse(localStorage.getItem("listPeople"));
     }
-    else {
-        listPeople = JSON.parse(localStorage.getItem('listPeople'));
-    }
-
-    let html = "";
-    listPeople.forEach(function (element, index) {
+    let html="";
+    listPeople.forEach(function(element, index){
         html += "<tr>";
-        html += "<td>" + element.email + "</td>";
-        html += "<td>" + element.nombre + "</td>";
-        html += "<td>" + element.numero + "</td>";
-        html += '<td><button onclick="deleteData(' + index + ')" class="btn btn-danger">Eliminar Dato</button><button onclick="updateData(' + index + ')" class="btn btn-warning">Editar Dato</button> </td>';
+        html += "<td>"+element.email+"</td>";
+        html += "<td>"+element.name+"</td>";
+        html += "<td>"+element.phone+"</td>";
+        html += '<td><button onclick="deleteData('+ index +')" class="btn btn-danger">Eliminar Dato</button> <button onclick="updateData('+index+')" class="btn btn-warning">Editar Dato</button></td>';
         html += "</tr>";
     });
-
     document.querySelector('#tableData tbody').innerHTML = html;
 
 }
-
 document.onload = showData();
 
 
-function addData() {
-    if (validateForm() == true) {
-        let email = document.getElementById('inputEmail').value;
-        let nombre = document.getElementById('inputNombre').value;
-        let numero = document.getElementById('inputNumero').value;
-        
-        let listPeople;
-        console.log(listPeople)
-        if (localStorage.getItem('listPeople') == null) {
-            listPeople = [];
-        }
-        else {
-            listPeople = JSON.parse(localStorage.getItem('listPeople'));
-            console.log(listPeople)
-            
-        }
+function addData(){
+    if(validateForm()==true){
+        //obtengo valores de los campos
+    let email = document.getElementById('inputEmail').value;
+    let name = document.getElementById('inputName').value;
+    let phone =document.getElementById('inputPhone').value;
 
-        listPeople.push({
-            email: email,
-            nombre: nombre,
-            numero: numero
-        });
-
-        localStorage.setItem('listPeople', JSON.stringify(listPeople));
-        showData();
-
-        document.getElementById('inputEmail').value= "";
-        document.getElementById('inputNombre').value= "";
-        document.getElementById('inputNumero').value= "";
-        console.log(listPeople)
-    }
-}
-
-function deleteData(index) {
     let listPeople;
 
-    if (localStorage.getItem('listPeople') == null) {
-        listPeople = [];
-    }
-    else {
-        listPeople = JSON.parse(localStorage.getItem('listPeople'));
+    if(localStorage.getItem('listPeople')==null){
+        listPeople=[];
+    }else{
+        listPeople = JSON.parse(localStorage.getItem("listPeople"));
     }
 
-    listPeople.splice(index, 1);
+    listPeople.push({
+        email:email,
+        name:name,
+        phone:phone,
+    });
+
     localStorage.setItem('listPeople', JSON.stringify(listPeople));
+    showData();
 
+    document.getElementById('inputEmail').value="";
+    document.getElementById('inputName').value="";
+    document.getElementById('inputPhone').value="";
+
+    }
+
+
+
+}
+function deleteData(index){
+    let listPeople;
+    if(localStorage.getItem('listPeople')==null){
+        listPeople=[];
+    }else{
+        listPeople = JSON.parse(localStorage.getItem("listPeople"));
+    }
+    listPeople.splice(index, 1);
+    localStorage.setItem('listPeople',JSON.stringify(listPeople));
     showData();
 
 }
-
-function updateData(index) {
-    document.getElementById('btnAdd').style.display = 'none';
-    document.getElementById('btnUpdate').style.display = 'block';
+function updateData(index){
+    document.getElementById("btnAdd").style.display='none';
+    document.getElementById("btnUpdate",btnAdd).style.display='block';
 
     let listPeople;
 
-    if (localStorage.getItem('listPeople') == null) {
-        listPeople = [];
+    if(localStorage.getItem('listPeople')==null){
+        listPeople=[];
+    }else{
+        listPeople = JSON.parse(localStorage.getItem("listPeople"));
     }
-    else {
-        listPeople = JSON.parse(localStorage.getItem('listPeople'));
-    }
-
+    //relleno el formulario con la data a editar
     document.getElementById('inputEmail').value = listPeople[index].email;
-    document.getElementById('inputNombre').value = listPeople[index].nombre;
-    document.getElementById('inputNumero').value = listPeople[index].numero;
+    document.getElementById('inputName').value = listPeople[index].name;
+    document.getElementById('inputPhone').value = listPeople[index].phone;
+    //funcion para actualizar datos
+    document.querySelector("#btnUpdate").onclick=function(){
+        if(validateForm()==true){
+            //actualizar datos de la lista
+            listPeople[index].email =document.getElementById('inputEmail').value
+            listPeople[index].name =document.getElementById('inputName').value
+            listPeople[index].phone =document.getElementById('inputPhone').value
 
-    document.querySelector('#btnUpdate').onclick= function () {
-        if (validateForm() == true){
-
-            listPeople[index].email.getElementById('inputEmail').value;
-            listPeople[index].nombre.getElementById('inputNombre').value;
-            listPeople[index].numero.getElementById('inputNumero').value;
-
+            //guardamos la lista actualizada en el localstorage
             localStorage.setItem('listPeople', JSON.stringify(listPeople));
+
+            //actualizamos la tabla
             showData();
+            document.getElementById('inputEmail').value="";
+            document.getElementById('inputName').value="";
+            document.getElementById('inputPhone').value="";
 
-            document.getElementById('inputEmail').value = "";
-            document.getElementById('inputNombre').value = "";
-            document.getElementById('inputNumero').value = "";
+            //cambiar visibilidad de los botones
+            document.getElementById("btnAdd").style.display='block';
+            document.getElementById("btnUpdate",btnAdd).style.display='none';
 
-            document.getElementById('btnAdd').style.display = 'block';
-            document.getElementById('btnUpdate').style.display = 'none';
         }
+    };
 
-
-    }
 }
