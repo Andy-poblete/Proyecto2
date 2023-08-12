dfe78ebd800a8f111f9a10637d0b89df1a6c2fc3
Andy-poblete <andy.poblete19@gmail.com>
2023-08-12 09:21:42 -0400

    aqui esta todo el proyecto

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-function Validador()
-{
-    let Nombre = document.getElementById('idNombre').value;
-    let Apellido = document.getElementById('idApellido').value;
+function validateForm() {
+    let email = document.getElementById('inputEmail').value;
+    let nombre = document.getElementById('inputNombre').value;
+    let numero = document.getElementById('inputNumero').value;
 
     //a continuacion se van a validar los campos
-    if(Nombre=="")
-    {
+    if (email == "") {
+        alert("Uno de los campos esta vacio");
+        return false;
+    }
+    else if(!email.includes('@')){
+        alert("Debe agregar un @ al campo");
         return false;
     }
 
-    if(Apellido=="")
-    {
+    else if (nombre == "") {
+        alert("Uno de los campos esta vacio");
+        return false;
+    }
+
+    else if (numero == "") {
+        alert("Uno de los campos esta vacio");
         return false;
     }
 
@@ -18,20 +27,118 @@ function Validador()
     return true;
 }
 
-function addData()
-{
-    if(validateForm()==true)
-    {
-        let Nombre = document.getElementById('idNombre').value;
-        let Apellido = document.getElementById('idApellido').value;
-        let listaRegistro;
+function showData() {
+    let listPeople;
+
+    if (localStorage.getItem('listPeople') == null) {
+        listPeople = [];
     }
-    if(localStorage.getItem('listaRegistro')==null)
-    {
-        listaRegistro=[];
+    else {
+        listPeople = JSON.parse(localStorage.getItem('listPeople'));
     }
-    else
-    {
-        listaRegistro = JSON.parse(localStorage.getItem("listaRegistro"));
+
+    let html = "";
+    listPeople.forEach(function (element, index) {
+        html += "<tr>";
+        html += "<td>" + element.email + "</td>";
+        html += "<td>" + element.nombre + "</td>";
+        html += "<td>" + element.numero + "</td>";
+        html += '<td><button onclick="deleteData(' + index + ')" class="btn btn-danger">Eliminar Dato</button><button onclick="updateData(' + index + ')" class="btn btn-warning">Editar Dato</button> </td>';
+        html += "</tr>";
+    });
+
+    document.querySelector('#tableData tbody').innerHTML = html;
+
+}
+
+document.onload = showData();
+
+
+function addData() {
+    if (validateForm() == true) {
+        let email = document.getElementById('inputEmail').value;
+        let nombre = document.getElementById('inputNombre').value;
+        let numero = document.getElementById('inputNumero').value;
+        
+        let listPeople;
+        console.log(listPeople)
+        if (localStorage.getItem('listPeople') == null) {
+            listPeople = [];
+        }
+        else {
+            listPeople = JSON.parse(localStorage.getItem('listPeople'));
+            console.log(listPeople)
+            
+        }
+
+        listPeople.push({
+            email: email,
+            nombre: nombre,
+            numero: numero
+        });
+
+        localStorage.setItem('listPeople', JSON.stringify(listPeople));
+        showData();
+
+        document.getElementById('inputEmail').value= "";
+        document.getElementById('inputNombre').value= "";
+        document.getElementById('inputNumero').value= "";
+        console.log(listPeople)
+    }
+}
+
+function deleteData(index) {
+    let listPeople;
+
+    if (localStorage.getItem('listPeople') == null) {
+        listPeople = [];
+    }
+    else {
+        listPeople = JSON.parse(localStorage.getItem('listPeople'));
+    }
+
+    listPeople.splice(index, 1);
+    localStorage.setItem('listPeople', JSON.stringify(listPeople));
+
+    showData();
+
+}
+
+function updateData(index) {
+    document.getElementById('btnAdd').style.display = 'none';
+    document.getElementById('btnUpdate').style.display = 'block';
+
+    let listPeople;
+
+    if (localStorage.getItem('listPeople') == null) {
+        listPeople = [];
+    }
+    else {
+        listPeople = JSON.parse(localStorage.getItem('listPeople'));
+    }
+
+    document.getElementById('inputEmail').value = listPeople[index].email;
+    document.getElementById('inputNombre').value = listPeople[index].nombre;
+    document.getElementById('inputNumero').value = listPeople[index].numero;
+
+    document.querySelector('#btnUpdate').onclick= function () {
+        if (validateForm() == true){
+
+            listPeople[index].email.getElementById('inputEmail').value;
+            listPeople[index].nombre.getElementById('inputNombre').value;
+            listPeople[index].numero.getElementById('inputNumero').value;
+
+            localStorage.setItem('listPeople', JSON.stringify(listPeople));
+            showData();
+
+            document.getElementById('inputEmail').value = "";
+            document.getElementById('inputNombre').value = "";
+            document.getElementById('inputNumero').value = "";
+
+            document.getElementById('btnAdd').style.display = 'block';
+            document.getElementById('btnUpdate').style.display = 'none';
+        }
+
+
     }
 }
